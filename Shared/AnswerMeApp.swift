88dd3a3c032fddfbada744d418602552ec31f354
Dan Hart <dhart94@gmail.com>
2021-08-21 17:46:45 -0500

    Initial Commit

---
+++ Shared/AnswerMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnswerMeApp.swift
+//  Shared
+//
+//  Created by Dan Hart on 8/21/21.
+//
+
+import SwiftUI
+
+@main
+struct AnswerMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
